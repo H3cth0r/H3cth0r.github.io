c7feff905e96c65c5c52d0416cd46c9079af4ec7
H3cth0r <hectorsucre13@gmail.com>
2022-03-08 14:17:31 -0600

    advacements

---
+++ sqlite_examples/jason_practice.py
@@ -0,0 +1,11 @@
+import json
+
+x = {
+    "name":"John",
+    "age":30,
+    "city":"new york"
+}
+
+y = json.dumps(x)
+
+print(y)
--- sqlite_examples/practice_002.py
+++ sqlite_examples/practice_002.py
@@ -1,12 +1,28 @@
+import collections
 import sqlite3
+import json
+
 mydb = sqlite3.connect("reto_PAS.db")
 cur = mydb.cursor()
 
-stringSQL = "SELECT Users.first_name, Party.date, Party.score FROM Users JOIN Party ON Users.id = Party.user;"
+stringSQL = '''SELECT Users.first_name, Party.date, Party.score FROM
+                 Users JOIN Party ON Users.id = Party.user'''
 
 rows = cur.execute(stringSQL)
+lista_salida = []
 
 for r in rows:
-    print(r[0], r[1], r[2])
+    d = collections.OrderedDict()
+    d["nombre"] = r[0]
+    d["fecha"]  = r[1]
+    d["puntaje"]= r[2]
+    lista_salida.append(d)
+
+j = json.dumps(lista_salida)
+print(j)
+
+f = open("salidaJSON.txt", "w")
+f.write(j)
+f.close()
 
 mydb.close()
